18ede40d780accc3a408856f92058095be810337
Dean <marsinellid@gmail.com>
2014-10-22 15:58:43 -0400

    added my entire page

---
+++ js/fadein.js
@@ -0,0 +1,11 @@
+/*
+  File: /~dmarsine/js/fadein.js
+  This file applies a quick fade in effect for pages
+  Author: Dean Marsinelli
+  Contact: marsinellid@gmail.com
+  Created on: October 20, 2014 
+*/
+
+$(document).ready( function() {
+  $("#allcontent").hide().fadeIn(500);
+});
